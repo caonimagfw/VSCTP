4b530779d9359c23f288b9fb2194b87ad2119324
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2019-11-11 14:36:39 +0800

    add all file

---
+++ CTPTradeApi-master/CTPTradeApiSimpleDemoVC2015/copy.bat
@@ -0,0 +1,23 @@
+xcopy ..\XTA_W32\Api\CTP_v6.3.6_20160606 Release\XTA_W32\Api\CTP_v6.3.6_20160606 /I /E /Y
+copy ..\XTA_W32\Cpp\XFinApi.ITradeApi.dll Release\XFinApi.ITradeApi.dll /Y
+
+xcopy ..\XTA_W32\Api\CTP_v6.3.6_20160606 Debug\XTA_W32\Api\CTP_v6.3.6_20160606 /I /E /Y
+copy ..\XTA_W32\Cpp\XFinApi.ITradeApid.dll Debug\XFinApi.ITradeApid.dll /Y
+
+xcopy ..\XTA_W64\Api\CTP_v6.3.6_20160606 x64\Release\XTA_W64\Api\CTP_v6.3.6_20160606 /I /E /Y
+copy ..\XTA_W64\Cpp\XFinApi.ITradeApi.dll x64\Release\XFinApi.ITradeApi.dll /Y
+
+xcopy ..\XTA_W64\Api\CTP_v6.3.6_20160606 x64\Debug\XTA_W64\Api\CTP_v6.3.6_20160606 /I /E /Y
+copy ..\XTA_W64\Cpp\XFinApi.ITradeApid.dll x64\Debug\XFinApi.ITradeApid.dll /Y
+
+xcopy ..\XTA_W32\Api\CTP_v6.3.13_20181119 Release\XTA_W32\Api\CTP_v6.3.13_20181119 /I /E /Y
+xcopy ..\XTA_W32\Api\CTP_v6.3.13_20181119 Debug\XTA_W32\Api\CTP_v6.3.13_20181119 /I /E /Y
+xcopy ..\XTA_W64\Api\CTP_v6.3.13_20181119 x64\Release\XTA_W64\Api\CTP_v6.3.13_20181119 /I /E /Y
+xcopy ..\XTA_W64\Api\CTP_v6.3.13_20181119 x64\Debug\XTA_W64\Api\CTP_v6.3.13_20181119 /I /E /Y
+
+xcopy ..\XTA_W32\Api\CTP_v6.3.15_20190220 Release\XTA_W32\Api\CTP_v6.3.15_20190220 /I /E /Y
+xcopy ..\XTA_W32\Api\CTP_v6.3.15_20190220 Debug\XTA_W32\Api\CTP_v6.3.15_20190220 /I /E /Y
+xcopy ..\XTA_W64\Api\CTP_v6.3.15_20190220 x64\Release\XTA_W64\Api\CTP_v6.3.15_20190220 /I /E /Y
+xcopy ..\XTA_W64\Api\CTP_v6.3.15_20190220 x64\Debug\XTA_W64\Api\CTP_v6.3.15_20190220 /I /E /Y
+
+pause
---
+++ FtdcNet.CTP-master/README.md
@@ -0,0 +1,23 @@
+## FtdcNet.CTP
+
+.Net wrapper for ftdc2c_ctp. Support x86/x64/AnyCPU.
+
+## License
+
+Licensed under the [MIT License](http://www.mit-license.org/)
+
+## Binary
+
+If you don't want to build yourself, just copy FtdcNet.CTP.dll, FtdcNet.CTP.xml, and x64 x86 directories from [Release](http://github.com/shawn666liu/FtdcNet.CTP/tree/master/Release) folder.
+
+## Version
+
+CTP version is V6.3.15 (2019-02-20)
+
+6.3.15 生产版API只能连接生产版看穿式监管前置
+
+## ChangeLog
+
+2019-05-12
+* ftdc2c_ctp integrated.
+* update to CTP v6.3.15
